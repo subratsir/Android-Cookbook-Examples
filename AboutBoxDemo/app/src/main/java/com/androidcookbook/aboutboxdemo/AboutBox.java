ab87826f7f2f0fa330c210133fb45087aa7209b6
MikeJuliet <52112400+MikeJuliet@users.noreply.github.com>
2020-06-16 16:51:08 +0200

    Migrated to AndroidX and updated dependencies

--- AboutBoxDemo/app/src/main/java/com/androidcookbook/aboutboxdemo/AboutBox.java
+++ AboutBoxDemo/app/src/main/java/com/androidcookbook/aboutboxdemo/AboutBox.java
@@ -13,44 +13,44 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class AboutBox {
-    static String VersionName(Context context) {
+    static String VersionName ( Context context ) {
         try {
-            return context.getPackageManager().getPackageInfo(
-                    context.getPackageName(), 0).versionName;
-        } catch (PackageManager.NameNotFoundException e) {
+            return context.getPackageManager ( ).getPackageInfo (
+                    context.getPackageName ( ), 0 ).versionName;
+        } catch ( PackageManager.NameNotFoundException e ) {
             return "Unknown";
         }
     }
 
-    public static void Show(Activity callingActivity) {
+    public static void Show ( Activity callingActivity ) {
         //Use a Spannable to allow for links highlighting
-        SpannableString aboutText = new SpannableString("Version " +
-                VersionName(callingActivity) + "\n\n" +
-                callingActivity.getString(R.string.about));
+        SpannableString aboutText = new SpannableString ( "Version " +
+                VersionName ( callingActivity ) + "\n\n" +
+                callingActivity.getString ( R.string.about ) );
         // views to pass to AlertDialog.Builder and to set the text
         View about;
         TextView tvAbout;
         try {
             //Inflate the custom view
-            LayoutInflater inflater = callingActivity.getLayoutInflater();
-            about = inflater.inflate(R.layout.aboutbox,
-                    (ViewGroup) callingActivity.findViewById(R.id.aboutView));
-            tvAbout = (TextView) about.findViewById(R.id.aboutText);
-        } catch (InflateException e) {
+            LayoutInflater inflater = callingActivity.getLayoutInflater ( );
+            about = inflater.inflate ( R.layout.aboutbox,
+                    ( ViewGroup ) callingActivity.findViewById ( R.id.aboutView ) );
+            tvAbout = ( TextView ) about.findViewById ( R.id.aboutText );
+        } catch ( InflateException e ) {
             // Unchecked exception, unlikely, but default to TextView if it occurs
-            about = tvAbout = new TextView(callingActivity);
+            about = tvAbout = new TextView ( callingActivity );
         }
 //Set the about text
-        tvAbout.setText(aboutText);
+        tvAbout.setText ( aboutText );
 // Now Linkify the text
-        Linkify.addLinks(tvAbout, Linkify.ALL);
+        Linkify.addLinks ( tvAbout, Linkify.ALL );
 //Build and show the dialog
-        new AlertDialog.Builder(callingActivity)
-                .setTitle("About " + callingActivity.getString(R.string.app_name))
-                .setCancelable(true)
-                .setIcon(R.drawable.icon)
-                .setPositiveButton( R.string.postiveButton, null)
-                .setView(about)
-                .show(); //Builder method returns allow for method chaining
+        new AlertDialog.Builder ( callingActivity )
+                .setTitle ( "About " + callingActivity.getString ( R.string.app_name ) )
+                .setCancelable ( true )
+                .setIcon ( R.drawable.icon )
+                .setPositiveButton ( R.string.positiveButton, null )
+                .setView ( about )
+                .show ( ); //Builder method returns allow for method chaining
     }
 }
